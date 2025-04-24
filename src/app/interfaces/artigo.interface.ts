659d2cb6db5d86c894040fec0451431a99f3a19e
Bráulio Dos Santos <dossantosbraulio20@gmail.com>
2025-04-24 18:16:29 +0100

    Adiciona propriedades à interface Artigo e ApiResponse

--- src/app/interfaces/artigo.interface.ts
+++ src/app/interfaces/artigo.interface.ts
@@ -2,4 +2,11 @@ export interface Artigo {
   id?: number;
   titulo: string;
   conteudo: string;
+  imagem?: string;
+}
+
+export interface ApiResponse<T> {
+  status: string;
+  data: T;  
+  message: string;
 }
