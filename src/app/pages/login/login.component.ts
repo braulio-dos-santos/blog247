58d28d2378394b37f9766f8fc64aa53200651edc
Bráulio Dos Santos <dossantosbraulio20@gmail.com>
2025-05-07 14:03:37 +0100

    feat: implementa formulário de login com autenticação e tratamento de erros

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  email = '';
+  password = '';
+  error = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: () => {
+        this.router.navigate(['/home']); // ou outro destino
+      },
+      error: (err) => {
+        this.error = err.error || 'Erro ao autenticar';
+      }
+    });
+  }
+
+  
+}
